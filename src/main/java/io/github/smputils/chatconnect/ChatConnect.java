a1e5bf9e9b8047c4db68c7fc8daf6e435898dd7f
Carlos Felgueiras <catofel2000@gmail.com>
2024-06-14 17:30:14 +0100

    feat(config): implemented config wrapper with default values and getters for the bot token, guild id and channel id

--- src/main/java/io/github/smputils/chatconnect/ChatConnect.java
+++ src/main/java/io/github/smputils/chatconnect/ChatConnect.java
@@ -2,13 +2,19 @@ package io.github.smputils.chatconnect;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.github.smputils.chatconnect.config.PluginConfig;
 import io.github.smputils.chatconnect.minecraft.ChatListener;
 
 public class ChatConnect extends JavaPlugin {
+
+    PluginConfig config;
+
     @Override
     public void onEnable() {
-        getLogger().info("Plugin Enabled!");
+        config = new PluginConfig(this);
+
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
+        getLogger().info("Plugin Enabled!");
     }
 
     @Override
